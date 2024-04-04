c3c3396bc17870b77f1ccd8d69348a5f96eb518a
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-04-04 18:12:15 -0500

    feat(project): :heavy_plus_sign: add React to Astro project

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,16 +4,31 @@ export default {
 	theme: {
 		extend: {
 			colors: {
-				'desaturated-red': 'hsl(0, 36%, 70%)',
-				'soft-red': 'hsl(0, 93%, 68%)',
-				'dark-grayish-red': 'hsl(0, 6%, 24%)',
+				'soft-red': 'hsl(7, 99%, 70%)',
+				'yellow': 'hsl(51, 100%, 49%)',
+				'dark-desaturated-cyan': 'hsl(167, 40%, 24%)',
+				'dark-blue': 'hsl(198, 62%, 26%)',
+				'dark-moderate-cyan': 'hsl(168, 34%, 41%)',
+				'very-dark-desaturated-blue': 'hsl(212, 27%, 19%)',
+				'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+				'dark-grayish-blue': 'hsl(232, 10%, 55%)',
+				'grayish-blue': 'hsl(210, 4%, 67%)',
 			},
 			backgroundColor: {
-				'desaturated-red': 'hsl(0, 36%, 70%)',
-				'soft-red': 'hsl(0, 93%, 68%)',
-				'dark-grayish-red': 'hsl(0, 6%, 24%)',
+				'soft-red': 'hsl(7, 99%, 70%)',
+				'yellow': 'hsl(51, 100%, 49%)',
+				'dark-desaturated-cyan': 'hsl(167, 40%, 24%)',
+				'dark-blue': 'hsl(198, 62%, 26%)',
+				'dark-moderate-cyan': 'hsl(168, 34%, 41%)',
+				'very-dark-desaturated-blue': 'hsl(212, 27%, 19%)',
+				'very-dark-grayish-blue': 'hsl(213, 9%, 39%)',
+				'dark-grayish-blue': 'hsl(232, 10%, 55%)',
+				'grayish-blue': 'hsl(210, 4%, 67%)',
 			},
-			
+			fontFamily: {
+				'fraunces': ['Fraunces', 'serif'],
+				'barlow': ['Barlow', 'sans-serif'],
+			},
 		},
 	},
 	plugins: [],
